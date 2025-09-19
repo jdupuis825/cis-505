689834125e25898d2e491ea9ea62a5f5a82b9d75
Jocelyn Dupuis <jdupuis@my365.bellevue.edu>
2025-09-19 08:55:05 -0400

    Assignment 2.2 Jar Files

---
+++ Module_2/JarPracticeFiles/DupuisJarTwo.java
@@ -0,0 +1,11 @@
+/*
+    Dupuis, J. (2025) CIS 505 Intermediate Java Programming. Bellevue University, all rights reserved.
+*/
+package JarPracticeFiles;
+
+public class DupuisJarTwo {
+    // First Jar Program
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
+}
