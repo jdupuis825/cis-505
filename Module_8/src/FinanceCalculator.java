e61f66471ca54618663e17519652e46b3395d745
Jocelyn Dupuis <jdupuis@my365.bellevue.edu>
2025-11-02 19:12:22 -0500

    Assignment 8.2 Enhanced Future Value App

---
+++ Module_8/src/FinanceCalculator.java
@@ -0,0 +1,20 @@
+package src;
+/*
+    Liang, Y.D. (2019). Introduction to Java Programming and Data Structures: 
+        Comprehensive Version (12th ed.). Pearson Education, Inc.
+    Dupuis, J. (2025). CSI 505 Intermediate Java Programming. Bellevue University. Modified by A. Student 2025.
+*/
+
+public class FinanceCalculator {
+
+    private static final int MONTHS_IN_YEAR = 12;
+
+    public static double calculateFutureValue(double monthlyPayment, double rate, int years) {
+        int months = years * MONTHS_IN_YEAR;
+        double interestRate = 1 + rate / 100;
+        double presentValue = monthlyPayment * months;
+        double futureValue = presentValue * Math.pow(interestRate, months);
+        return futureValue;
+    }
+}
+
